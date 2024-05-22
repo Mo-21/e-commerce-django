26c02ee47427d4cc3be2e4066c3e437fad641886
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-22 06:06:00 +0300

    create Product model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from decimal import Decimal
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    slug = models.SlugField()
+    unit_price = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(Decimal(1))])
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10000)])

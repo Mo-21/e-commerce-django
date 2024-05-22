8ff186ee1a2b0b76818cf7cf811eeb9c87fda29d
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-22 19:50:46 +0300

    create cart and cart_item models

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
+from uuid import uuid4
 from decimal import Decimal
 
 
@@ -40,3 +41,18 @@ class Customer(models.Model):
 
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart, on_delete=models.PROTECT, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
+
+    class Meta:
+        unique_together = [['product', 'cart']]

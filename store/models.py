6bb04a6362a04ea79217614563a797295d0eb5fd
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-22 20:26:14 +0300

    create order and orderItem models

--- store/models.py
+++ store/models.py
@@ -43,6 +43,29 @@ class Customer(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
+class Order(models.Model):
+    PAYMENT_PENDING = 'P'
+    PAYMENT_FAILED = 'F'
+    PAYMENT_COMPLETE = 'C'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_PENDING, 'Pending'),
+        (PAYMENT_FAILED, 'Failed'),
+        (PAYMENT_COMPLETE, 'Complete')
+    ]
+    payment_status = models.CharField(
+        max_length=1, default='P', choices=PAYMENT_STATUS_CHOICES)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
+    unit_price = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(Decimal(1))])
+
+
 class Cart(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)

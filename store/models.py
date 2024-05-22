2b73d4a55d3e77817ddc72acf82f3621280a67b8
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-22 06:16:38 +0300

    create collection model

--- store/models.py
+++ store/models.py
@@ -3,6 +3,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from decimal import Decimal
 
 
+class Collection(models.Model):
+    name = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
@@ -11,3 +17,5 @@ class Product(models.Model):
         max_digits=6, decimal_places=2, validators=[MinValueValidator(Decimal(1))])
     quantity = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(10000)])
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    last_update = models.DateTimeField(auto_now=True)

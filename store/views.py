4b53cffbbd2a340448414aeb9f5e1db60ca2cae1
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-23 04:31:20 +0300

    apply product ordering

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
 from django.db.models.aggregates import Count
 from .models import Collection, Product
 from .pagination import CustomPagination
@@ -16,4 +17,7 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related(
         'collection').prefetch_related('promotion')
     serializer_class = serializers.ProductSerializer
+    filter_backends = [OrderingFilter]
     pagination_class = CustomPagination
+    ordering_fields = ['id', 'title', 'unit_price',
+                       'quantity']

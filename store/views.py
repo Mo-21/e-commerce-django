f3c8a7d1bebbcb268194ea8934efc668d79f9ea0
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-23 04:46:10 +0300

    apply product filtering with django_filters

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
+from django.db.models.aggregates import Count
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import OrderingFilter
-from django.db.models.aggregates import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Collection, Product
 from .pagination import CustomPagination
+from .filters import ProductFilter
 from . import serializers
 
 
@@ -17,7 +19,8 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related(
         'collection').prefetch_related('promotion')
     serializer_class = serializers.ProductSerializer
-    filter_backends = [OrderingFilter]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     pagination_class = CustomPagination
     ordering_fields = ['id', 'title', 'unit_price',
                        'quantity']
+    filterset_class = ProductFilter

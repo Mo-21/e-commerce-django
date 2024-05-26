b556198d5cdf18d58eff6989c7b8bbb353912587
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-26 04:00:40 +0300

    delete cart items when deleting product or order (CASCADE)

--- store/models.py
+++ store/models.py
@@ -84,8 +84,8 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     cart = models.ForeignKey(
-        Cart, on_delete=models.PROTECT, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+        Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(validators=[MinValueValidator(1)])
 
     class Meta:

e4de7a8c6b85a75028ccbac43ce86df4bee11b53
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-27 01:32:19 +0300

    add inline tags to products

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 from .models import User
+from tags.models import TaggedItem
+from store.admin import ProductAdmin
+from store.models import Product
 
 
 @admin.register(User)
@@ -10,3 +14,17 @@ class UserAdmin(admin.ModelAdmin):
 
     list_filter = ['is_staff']
     list_per_page = 20
+
+
+class TagInline(GenericTabularInline):
+    model = TaggedItem
+    autocomplete_fields = ['tag']
+    extra = 0
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = [TagInline]
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)

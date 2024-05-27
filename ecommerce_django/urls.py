0055198789099e58a32165cc027ecd36a9d56db7
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-27 05:42:42 +0300

    add a homepage

--- ecommerce_django/urls.py
+++ ecommerce_django/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('store/', include('store.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),

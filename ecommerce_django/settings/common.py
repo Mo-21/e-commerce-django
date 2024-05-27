ca94e6cc88c51606cf523355a12c7db6ffe78d83
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-27 22:51:20 +0300

    dockerize the application

--- ecommerce_django/settings/common.py
+++ ecommerce_django/settings/common.py
@@ -141,16 +141,6 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "TIMEOUT": 10*60,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
 
 LOGGING = {
     'version': 1,
